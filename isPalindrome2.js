55f7820f4b678da8846b2e08c5242f6bf507ee94
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-04 11:27:03 +0700

    ver 2 of 4th problem

--- isPalindrome2.js
+++ isPalindrome2.js
@@ -12,4 +12,4 @@ while(x>revertedNum){
 // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
 // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
  // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
- console.log(isPalindrome(12322))
+ 
