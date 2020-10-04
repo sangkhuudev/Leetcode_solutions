4d46a01aca20324b2d1a6895736199adfc876e8f
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-04 10:07:50 +0700

    Quick update

--- twosum.js
+++ twosum.js
@@ -11,7 +11,5 @@ const nums=[3,4,5,2];  // to compute 2 numbers in array set = target
     }
     return result;
 }
-console.log(twoSum(nums,8));
-console.log(twoSum(nums,6));
-console.log(twoSum(nums,5));
+
 
