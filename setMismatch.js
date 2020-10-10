667e78a0953f5091e12f483040e498d1aebe4275
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-10 15:28:23 +0700

    24th solved

---
+++ setMismatch.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function findErrorNum(nums){
+    let hash=nums.reduce((acc,num)=>{
+        acc[num]=acc[num]+1 || 1;
+        return acc
+    },{});
+    let sum=nums.reduce((acc,num)=>{return acc+num},0); 
+    let checkSum=nums.length*(nums.length+1)/2
+    let distance= sum-checkSum 
+   for(let i=0;i<nums.length;i++){ 
+        if(hash[nums[i]]!=1){
+            return [nums[i],nums[i]-distance];        
+        }      
+    }  
+}
+
