a1d1ea8480d88f3ab7377e491e7b19df29ce4eb7
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-11 19:24:10 +0700

    Update transposeMatrix.js

--- transposeMatrix.js
+++ transposeMatrix.js
@@ -2,9 +2,8 @@
  * @param {number[][]} A
  * @return {number[][]}
  */
-const a=[[1,2,3],[4,5,6],[7,8,9]];// [[1,4,7],[2,5,8],[3,6,9]]
 function transpose(A){
-    let matrix=[]
+    let matrix=new Array(A[0].length).fill(Array(A.length).fill(0))
     let i=-1;
     while(++i<A[0].length){
         if(!matrix[i]) matrix[i]=[];
@@ -15,4 +14,7 @@ function transpose(A){
     }
     return matrix
 }
-console.log(transpose(a))
+// ver 2 : using map
+function transpose2(A){
+    return A[0].map((val,ind)=>A.map(row=>row[ind]))
+}
