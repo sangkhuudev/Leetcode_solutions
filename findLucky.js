37557226d39156795ce7af3e0528105af6d6c7df
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-16 10:08:28 +0700

    63rd revised

--- findLucky.js
+++ findLucky.js
@@ -5,7 +5,8 @@
 function findLucky(arr){
     let hashmap=new Map();
     for(let item of arr) {
-       hashmap.has(item) ? hashmap.set(item,hashmap.get(item)+1):hashmap.set(item,1)
+      // hashmap.has(item) ? hashmap.set(item,hashmap.get(item)+1):hashmap.set(item,1)
+       hashmap.set(item,1+(hashmap.get(item)||0));
     }
     return Math.max(...arr.filter(key=>hashmap.get(key)==key),-1)  
 }
