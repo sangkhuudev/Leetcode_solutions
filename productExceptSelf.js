deb996c880b58aab7679842bc5df0a0742813808
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-16 15:05:57 +0700

    productExceptSelf revised

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -3,18 +3,15 @@
  * @return {number[]}
  */
 function productExceptSelf(nums){
-   let leftProd=[],rightProd=[];
-   let res=[]
-   leftProd[0]=1;
-   for(let i=1;i<nums.length;i++){
-        leftProd[i]=leftProd[i-1]*nums[i-1]
-   }
-   rightProd[nums.length-1]=1;
-   for(let i=nums.length-2;i>=0;i--){
-       rightProd[i]=rightProd[i+1]*nums[i+1]
-   }
-   for(let i=0;i<nums.length;i++){
-       res[i]=leftProd[i]*rightProd[i]
-   }
-   return res
+    let res=[]
+    res[0]=1;
+    for(let i=1;i<nums.length;i++){
+         res[i]=res[i-1]*nums[i-1]
+    }
+    let rightProd=1
+    for(let i=nums.length-1;i>=0;i--){
+        res[i]=res[i]*rightProd;
+        rightProd=rightProd*nums[i]
+    }
+    return res
 }  
