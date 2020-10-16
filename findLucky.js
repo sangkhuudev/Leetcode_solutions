d32b27e46408924c9cd3d5509bb2904d1dceb543
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-16 09:58:09 +0700

    63rd solved

---
+++ findLucky.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function findLucky(arr){
+    let hashmap=new Map();
+    for(let item of arr) {
+       hashmap.has(item) ? hashmap.set(item,hashmap.get(item)+1):hashmap.set(item,1)
+    }
+    return Math.max(...arr.filter(key=>hashmap.get(key)==key),-1)  
+}
