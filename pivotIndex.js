798b5537762adb35ebd167d67ffd5ce9c818313e
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-16 18:31:12 +0700

    pivotIndex resived

--- pivotIndex.js
+++ pivotIndex.js
@@ -4,13 +4,11 @@
  */
 // technique of prefix sum
 function pivotIndex(nums){
-    let n=nums.length
-    let leftSum=Array(n).fill(0);
-    let rightSum=Array(n).fill(0);
-    for(let i=1;i<n;i++) leftSum[i]=leftSum[i-1]+nums[i-1];
-    for(let j=n-2;j>=0;j--) rightSum[j]=rightSum[j+1]+nums[j+1];
-    for(let i=0;i<n;i++){
-        if(leftSum[i]==rightSum[i]) return i
+    let sum=nums.reduce((acc,item)=>acc+item,0);
+    let currentSum=0
+    for(let i=0;i<nums.length;;i++){
+        if((sum-nums[i])/2==currentSum) return i;
+        else currentSum+=nums[i]
     }
     return -1
 }
