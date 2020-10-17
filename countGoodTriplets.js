1c7ea67df0a63a2fef7a0c036cc5d81527876ab0
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-17 15:25:45 +0700

    76th solved

---
+++ countGoodTriplets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+function countGoodTriplets(arr,a,b,c){
+    let res = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        
+        for(let j = i+1; j < arr.length; j++) {
+            if(Math.abs(arr[i] - arr[j]) > a) continue;
+            
+            for(let k = j+1; k < arr.length; k++) {
+                if(Math.abs(arr[j] - arr[k]) > b) continue;
+                if(Math.abs(arr[k] - arr[i]) <= c) res++;
+            }
+        }
+    }
+    return res;
+}
