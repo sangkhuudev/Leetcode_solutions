a20236006b6edd858e622eb511f4c939f78fca9d
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-26 10:12:55 +0700

    107th solved

---
+++ restoreMatrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} rowSum
+ * @param {number[]} colSum
+ * @return {number[][]}
+ */
+function restoreMatrix(rowSum,colSum){
+     let matrix=new Array(rowSum.length).fill(0).map(row=> new Array(colSum.length).fill(0))
+     for(let i=0;i<rowSum.length;i++){
+         for(let j=0;j<colSum.length;j++){
+             matrix[i][j]=Math.min(rowSum[i],colSum[j]);
+             rowSum[i]-=matrix[i][j];
+             colSum[j]-=matrix[i][j]
+         }
+     }
+     return matrix
+}
+const rowSum = [5,7,10];
+const colSum = [8,6,8];
+console.log(restoreMatrix(rowSum,colSum))
