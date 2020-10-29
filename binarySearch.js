17d41c3248e960caa4a64e84381ead103d2f0339
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-10-29 17:53:14 +0700

    120th solved

---
+++ binarySearch.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function binarySearch(nums,target){
+    let low=0;
+    let high= nums.length-1;
+    while(low<=high){
+        let mid= Math.floor((high+low)/2);
+        if(nums[mid]==target) return mid;
+        else if( nums[mid]>target) high=mid-1;
+        else low=mid+1
+    }
+    return -1
+}
