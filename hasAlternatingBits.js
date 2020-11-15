c11d8a539dc3bde09b11d576ae39d60925588863
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-11-15 18:08:06 +0700

    147th solved

---
+++ hasAlternatingBits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+// ver 1
+function hasAlternatingBits(n){
+    let arr= n.toString(2).split('').map(Number);
+    console.log(arr)
+    for(let i=0; i+1<arr.length;++i){
+        if(arr[i]==arr[i+1]) return false;
+    }
+    return true
+}
+// ver 2
+function hasAlternatingBits2(n){
+    while(n > 0){
+        let lastBit= (n & 1);
+        n= (n>>1);
+        if(lastBit === (n & 1)) return false;
+    }
+    return true
+}
