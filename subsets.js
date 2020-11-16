c3dca7c61460de9aadf35b7fa09ce869f1f66d33
sangkhuudev <72292079+sangkhuudev@users.noreply.github.com>
2020-11-16 12:48:35 +0700

    Add 3rd method of Subsets

--- subsets.js
+++ subsets.js
@@ -7,12 +7,12 @@ function subsets(nums){
     let set=new Set()
     set.add([])
     for(let i=0;i<nums.length;i++){
-       let subsets=[...set];
-       subsets.forEach(subset=>{
-           let newSubset=subset.slice(0);
-           newSubset.push(nums[i]);
-           set.add(newSubset)
-       }); 
+        let subsets=[...set];
+        subsets.forEach(subset=>{
+            let newSubset=subset.slice(0);
+            newSubset.push(nums[i]);
+            set.add(newSubset)
+        }); 
     }
     return [...set]
 }
@@ -27,4 +27,16 @@ function subsets2(nums){
         }
     }
     return res
+}
+// ver 3: using bit manipulation
+function subsets3(nums){
+    let powerSet= [];
+    for(let i=0; i< (1<<nums.length); ++i){
+        let subset = []
+        for(let j=0;j< nums.length;++j){
+            if( i &(1<<j)) subset.push(nums[j])
+        }
+        powerSet.push(subset)
+    }
+    return powerSet
 }
